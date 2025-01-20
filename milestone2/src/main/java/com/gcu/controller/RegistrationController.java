6a02b3709cd938570b5bbfab9d4bd3e4d1de78c7
Ackley <125339413+AckleyGCU@users.noreply.github.com>
2025-01-19 20:05:08 -0500

    Registration Page

--- milestone2/src/main/java/com/gcu/controller/RegistrationController.java
+++ milestone2/src/main/java/com/gcu/controller/RegistrationController.java
@@ -1,14 +1,37 @@
 package com.gcu.controller;
 
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.gcu.model.RegistrationModel;
 
 @Controller
-public class RegistrationController {
+public class RegistrationController implements WebMvcConfigurer {
 	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/mainPage").setViewName("mainPage");
+	}
+
 	@GetMapping("/register")
-	public String showRegistration() {
+	public String showRegistration(RegistrationModel registrationModel) {
 		return "registrationPage";
 	}
 
+	@PostMapping("/register")
+	public String checkPersonInfo(@Valid RegistrationModel registrationModel, BindingResult bindingResult) {
+		System.err.println(bindingResult.hasErrors());
+		System.err.println(registrationModel);
+		if (bindingResult.hasErrors()) {
+			return "registrationPage";
+		}
+
+		return "redirect:/main";
+	}
 }
--- milestone2/src/main/java/com/gcu/model/RegistrationModel.java
+++ milestone2/src/main/java/com/gcu/model/RegistrationModel.java
@@ -1,5 +1,77 @@
 package com.gcu.model;
 
-public class RegistrationModel {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
+
+public class RegistrationModel {
+    @NotNull(message = "Don't leave blank!")
+    @Size(min=2, max=30, message = "First name must be between 2 and 30 characters!")
+    private String firstName;
+    @NotNull(message = "Don't leave blank!")
+    @Size(min=2, max=30, message = "Last name must be between 2 and 30 characters!")
+    private String lastName;
+    @NotNull(message = "Don't leave blank!")
+    @Size(min=2, max=30, message = "Username must be between 2 and 30 characters!")
+    private String username;
+    @NotNull(message = "Don't leave blank!")
+    @Size(min=2, max=30, message = "Password must be between 2 and 30 characters!")
+    private String password;
+    @NotNull(message = "Don't leave blank!")
+    @Size(min=2, max=45, message = "Email must be between 2 and 45 characters!")
+    @Email(message = "You must enter a valid email!")
+    private String email;
+    @NotNull(message = "Don't leave blank!")
+    @Pattern(regexp="^(\\d{3}[- .]?){2}\\d{4}$", message = "You must enter a valid phone number!")
+    private String phoneNumber;
+
+    public String getFirstName(){
+        return this.firstName;
+    } 
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    } 
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername(){
+        return this.username;
+    } 
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    } 
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return this.email;
+    } 
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    } 
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
