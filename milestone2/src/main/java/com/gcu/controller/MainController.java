fb374e0a09539dd2b5091ea7d5082ab6ac2b7ce3
Andrew Forster <forsterandrew15@gmail.com>
2025-01-19 21:46:44 -0700

    Fixed bug with navbar introduced from previous commits. Other minor changes.

--- milestone2/src/main/java/com/gcu/controller/MainController.java
+++ milestone2/src/main/java/com/gcu/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping("/main")
+	@GetMapping({"/", "/main", "/home"})
 	public String showTitle() {
 		return "mainPage";
 	}
