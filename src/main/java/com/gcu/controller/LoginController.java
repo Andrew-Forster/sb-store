554eaf0a7ae08c4edfb6c85c841af24287bb42b7
Dmitry879 <dvaskovich@my.gcu.edu>
2025-02-22 02:32:31 -0500

    Modified LoginController class
    - Removed unnecessary imports.

--- src/main/java/com/gcu/controller/LoginController.java
+++ src/main/java/com/gcu/controller/LoginController.java
@@ -1,13 +1,7 @@
 package com.gcu.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-import com.gcu.model.LoginModel;
-import com.gcu.service.UserLoginService;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.GetMapping; 
 
 @Controller
 public class LoginController {
